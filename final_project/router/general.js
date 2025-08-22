aa693e4fccb3163f4ee780b8e2fc5c80ad973719
OmarGalindoJ <ingomargalindo@outlook.com>
2025-08-22 02:14:02 -0400

    Update routes on general.js file

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,7 +13,7 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  res.send(JSON.stringify(books,null,4));
+    res.send(books);
 });
 
 // Get book details based on ISBN
@@ -27,19 +27,35 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author; 
+  let book = [];
+    Object.keys(books).forEach(i => {
+       if(books[i].author.toLowerCase() == author.toLowerCase()){
+           book.push(books[i])
+       }
+   });
+    res.send(book)
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    //Write your code here
+    // Retrieve the isbn parameter from the request URL and send the corresponding book's details
+    const title = req.params.title;
+    let book = [];
+    Object.keys(books).forEach(i => {
+       if(books[i].title.toLowerCase() == title.toLowerCase()){
+            book.push(books[i])
+       }
+    });
+    res.send(book)
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    //Write your code here
+    const isbn = req.params.isbn;
+    res.send(books[isbn].reviews)
 });
 
 module.exports.general = public_users;
