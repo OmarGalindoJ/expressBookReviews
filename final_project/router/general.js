0c91aa29c90c0dde94c73b3013a737474d646886
OmarGalindoJ <ingomargalindo@outlook.com>
2025-08-24 00:33:05 -0400

    Modify code for async/await and promises function on general.js file

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,24 +18,52 @@ public_users.post("/register", (req,res) => {
       return res.status(404).json({message: "User already exists, please try again with a different username."});    
     }
   } 
-    return res.status(404).json({message: "A error ocurred when trying to register user, please try again."});
+  return res.status(404).json({message: "A error ocurred when trying to register user, please try again."});
 });
 
-// Get the book list available in the shop
+// Function with a Promise to be called for async GET requests
+function getBooksPromise(booksRouter) { 
+    return new Promise((resolve, reject) => {
+        if (booksRouter) {
+            resolve(booksRouter);
+        } else {
+            reject("No books were found, please try again with different parameters.");
+        }
+    });
+}
+
+
+/*Task 1: Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
     res.send(books);
+});*/
+
+// Task 10: Get the list of books available in the shop by async/await
+public_users.get('/', async function (req, res) {
+    let bookList = await getBooksPromise(books);
+    res.send(bookList);
 });
 
-// Get book details based on ISBN
+/*Task 2: Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
     // Retrieve the isbn parameter from the request URL and send the corresponding book's details
     const isbn = req.params.isbn;
     res.send(books[isbn]);
+ });*/
+
+ //Task 11: Get book details based on ISBN by Promise
+public_users.get('/isbn/:isbn', function (req, res) {
+    const isbn = req.params.isbn;
+    getBooksPromise(books[isbn])
+    .then(
+        result => res.send(result),
+        error => res.send(error)
+    )
  });
-  
-// Get book details based on author
+
+/*Task 3: Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
   const author = req.params.author; 
@@ -46,9 +74,23 @@ public_users.get('/author/:author',function (req, res) {
        }
    });
     res.send(book)
+});*/
+
+//Task 12: Get book details based on author by async/await
+public_users.get('/author/:author', async function (req, res) {
+    const author = req.params.author;
+    let book = [];
+    let bookList = await getBooksPromise(books);
+
+    Object.keys(bookList).forEach(i => {
+        if(bookList[i].author.toLowerCase() == author.toLowerCase()){
+            book.push(books[i])
+        }
+    });
+    res.send(book);
 });
 
-// Get all books based on title
+/* Task4: Get all books based on title
 public_users.get('/title/:title',function (req, res) {
     //Write your code here
     // Retrieve the isbn parameter from the request URL and send the corresponding book's details
@@ -60,6 +102,20 @@ public_users.get('/title/:title',function (req, res) {
        }
     });
     res.send(book)
+});*/
+
+// Task 13: Get book details based on title by async/await
+public_users.get('/title/:title', async function (req, res) {
+    const title = req.params.title;
+    let book = [];
+    let bookList = await getBooksPromise(books);
+
+    Object.keys(bookList).forEach(i => {
+        if(bookList[i].title.toLowerCase() == title.toLowerCase()){
+            book.push(bookList[i])
+        }
+    });
+    res.send(book);
 });
 
 //  Get book review
