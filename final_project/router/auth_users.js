e31e767b7f104bb90b3b3dfe70e1a19e3a6198c4
OmarGalindoJ <ingomargalindo@ouitlook.com>
2025-08-24 02:42:54 -0400

    Correct code for tasks 8 an 9 on auth_users.js file

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,72 +5,74 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
-    let filtered_users = users.filter((user)=> user.username === user);
-        if(filtered_users){
-    return true;
-    }
-    return false;
-    }
-
-const authenticatedUser = (username,password)=>{ //returns boolean
-    if(isValid(username)){
-        let filtered_users = users.filter((user)=> (user.username===username)&&(user.password===password));
-        if(filtered_users){
-            return true;
-        }
+const isValid = (username) => {
+    let userswithsamename = users.filter((user) => {
+        return user.username === username
+    });
+    if (userswithsamename.length > 0) {
+        return true;
+    } else {
         return false;
-       
     }
-    return false;
-    
 }
 
-//only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-    let user = req.body.username;
-    let pass = req.body.password;
-    if(!authenticatedUser(user,pass)){
-        return res.status(403).json({message:"User not authenticated"})
+const authenticatedUser = (username, password) => {
+    let validusers = users.filter((user) => {
+        return (user.username === username && user.password === password)
+    });
+    if (validusers.length > 0) {
+        return true;
+    } else {
+        return false;
     }
+}
 
-    let accessToken = jwt.sign({
-      data: user
-    },'access',{expiresIn:60*60})
-    req.session.authorization = {
-      accessToken
+// Task 7: Logging in as a registered user
+regd_users.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    if (!username || !password) {
+        return res.status(404).json({ message: "Error logging in, please try again." });
     }
-    res.send("User logged in successfully")
+    if (authenticatedUser(username, password)) {
+        let accessToken = jwt.sign({
+            data: password
+        }, 'access', { expiresIn: 60 * 60 });
+        req.session.authorization = {
+            accessToken, username
+        }
+        return res.status(200).send("User successfully logged in!");
+    } else {
+        return res.status(208).json({ message: "Invalid Login, please check username and password and try again." });
+    }
 });
 
-/* Add a book review
+// Task 8: Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  const isbn = req.params.isbn;
-  const username = req.session.authorization.username;
-  const review = req.body.review;
+    const isbn = req.params.isbn;
+    const username = req.session.authorization.username;
+    const review = req.body.review;
 
-  if (books[isbn]){
-      books[isbn].reviews[username] = review;
-      res.send(`The review of the book with ISBN ${isbn} from user ${username} has been published.`);
-  } else {
-      res.send(`No books with ISBN ${isbn} were found in the database.`);
-  }
-    });
+    if (books[isbn]){
+        books[isbn].reviews[username] = review;
+        res.send(`The review of the book with ISBN ${isbn} from user ${username} has been published.`);
+    } else {
+        res.send(`No books with ISBN ${isbn} were found in the database.`);
+    }
+});
 
-    regd_users.delete("/auth/review/:isbn", (req, res) => {
-        const isbn = req.params.isbn;
-        const username = req.session.authorization.username;
-        
-        if (books[isbn].reviews[username]){
-            delete books[isbn].reviews[username];
-            res.send(`The review of the book with ISBN ${isbn} from user ${username} has been deleted.`);
-        } else {
-            res.send(`No reviews with ISBN ${isbn} from user ${username} were found in the database.`);
-        }
-    });*/
+// Task 9: Delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    const username = req.session.authorization.username;
+    
+    if (books[isbn].reviews[username]){
+        delete books[isbn].reviews[username];
+        res.send(`The review of the book with ISBN ${isbn} from user ${username} has been deleted.`);
+    } else {
+        res.send(`No reviews with ISBN ${isbn} from user ${username} were found in the database.`);
+    }
+});
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
